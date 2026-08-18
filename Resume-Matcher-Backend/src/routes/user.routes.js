90ccd781d2e9120be2e194e739255db2a0f93a3a
swapnil_1800 <swapnilsureshgupta365@gmail.com>
2026-08-19 00:59:24 +0530

    controller for the user

---
+++ Resume-Matcher-Backend/src/routes/user.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  getCurrentUser,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+
+// secured routes - require a valid access token
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+export default router;
