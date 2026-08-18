51e886b6af05deb61498458873dbf1da05c4d630
swapnil_1800 <swapnilsureshgupta365@gmail.com>
2026-08-19 01:07:03 +0530

    upadating routes

--- Resume-Matcher-Backend/src/routes/user.routes.js
+++ Resume-Matcher-Backend/src/routes/user.routes.js
@@ -13,7 +13,7 @@ const router = Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
-// secured routes - require a valid access token
+// secured routes //
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
